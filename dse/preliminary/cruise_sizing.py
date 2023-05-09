a328f573ff6e68db744abc5aa679eb72ab1d142f
jalonso <63980841+javialonso05@users.noreply.github.com>
2023-05-09 16:09:30 +0200

    Change in wing area with flight velocity (400km/h and Mach 0.7) are now explored

--- dse/preliminary/cruise_sizing.py
+++ dse/preliminary/cruise_sizing.py
@@ -17,26 +17,28 @@ cl = 1.5
 cd = 0.11
 
 # Obtain data for S and gamma_e
-rho = 0.01
-V = 112
-q = 0.5*rho*V**2
-results_list = list()
-for y in range(0, 91):
-    A = np.array([[cl*q, np.sin(np.radians(y))],
-                  [-cd*q, np.cos(np.radians(y))]])
-    B = np.array([MTOM*g_mars, 0])
-    S, T = np.linalg.solve(A, B)
-    results_list.append([y, S, T])
-
-results_arr = np.array(results_list).T
-
 fig, (ax1, ax2) = plt.subplots(2, 1, sharex='all')
 fig.subplots_adjust(hspace=0)
 
-ax1.plot(results_arr[0], results_arr[1])
+rho = 0.01
+for V in [112, 0.7*220]:
+    q = 0.5*rho*V**2
+    results_list = list()
+    for y in range(0, 91):
+        A = np.array([[cl*q, np.sin(np.radians(y))],
+                      [-cd*q, np.cos(np.radians(y))]])
+        B = np.array([MTOM*g_mars, 0])
+        S, T = np.linalg.solve(A, B)
+        results_list.append([y, S, T])
+
+    results_arr = np.array(results_list).T
+
+    ax1.plot(results_arr[0], results_arr[1], label=f'V = {round(V, 1)}')
+    ax2.plot(results_arr[0], results_arr[2])
+
 ax1.set(ylabel=r'Wing area [m$^2$]')
+ax1.legend()
 ax1.grid()
-ax2.plot(results_arr[0], results_arr[2])
 ax2.plot([results_arr[0][0], results_arr[0][-1]], [max_thrust, max_thrust])
 ax2.set(xlabel='Thrust deflection [deg]', ylabel='Thrust [N]')
 ax2.grid()
