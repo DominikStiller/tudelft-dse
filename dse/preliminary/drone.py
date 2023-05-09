97e08e65c3a3b276d8bac5193d9ca0aa77d35caa
Dominik Stiller <domi.stiller@gmail.com>
2023-05-09 16:31:59 +0200

    Use thrust per rotor

--- dse/preliminary/drone.py
+++ dse/preliminary/drone.py
@@ -21,8 +21,8 @@ n_rotors = 4
 Cd = 0.03  # avg drag coefficient
 
 
-def velocity_induced_hover(T, rho, A):
-    return np.sqrt(T / (2 * rho * A))
+def velocity_induced_hover(T_per_rotor, rho, A):
+    return np.sqrt(T_per_rotor / (2 * rho * A))
 
 
 def velocity_induced_vertical_climb(T, rho, A, V_c):
@@ -50,17 +50,22 @@ def omega_from_rpm(rpm):
     return rpm / 60 * 2 * np.pi
 
 
-def _thrust_single_blade(rho, A, SR, a, theta_0, theta_tw, R, alpha, vi, omega, V):
-    t1 = (1 / 6) * rho * A * SR * a * theta_0 * R**2 + (1 / 8) * SR * a * rho * A * theta_tw * R**2
+def _thrust_single_rotor(rho, A, SR, a, theta_0, theta_tw, R, alpha, vi, omega, V):
+    t1 = (1 / 6) * rho * A * SR * a * theta_0 * R**2 + (
+        1 / 8
+    ) * SR * a * rho * A * theta_tw * R**2
     t2 = (1 / 4) * rho * A * SR * a * theta_0 + (1 / 8) * SR * a * rho * A * theta_tw
     t3 = (-1 / 4) * SR * a * rho * A * R
     T = (omega**2) * t1 + ((V * np.cos(alpha)) ** 2) * t2 + omega * (vi + V * np.sin(alpha)) * t3
     return T
 
+
 def rotortorque(rho, A, SR, a, theta_0, theta_tw, R, alpha, vi, omega, V):
     q1 = (1 / 8) * rho * A * SR * Cd * R**3
     q2 = (1 / 8) * rho * A * SR * Cd
-    q3 = (1 / 4) * SR * a * rho * A * theta_0 * R**2 + (1 / 8) * rho * A * SR * a * theta_tw * R**2
+    q3 = (1 / 4) * SR * a * rho * A * theta_0 * R**2 + (
+        1 / 8
+    ) * rho * A * SR * a * theta_tw * R**2
     q4 = (-1 / 4) * SR * a * rho * A * R
     Q = (
         (omega**2) * q1
@@ -70,6 +75,7 @@ def rotortorque(rho, A, SR, a, theta_0, theta_tw, R, alpha, vi, omega, V):
     )
     return Q
 
+
 def solve_thrust_hover(R=R, omega=omega):
     def f(T):
         A = area(R)
@@ -77,12 +83,12 @@ def solve_thrust_hover(R=R, omega=omega):
         vi = velocity_induced_hover(T, rho, A)
         V = 0
         alpha = 0
-        T_calc = _thrust_single_blade(rho, A, SR, a, theta_0, theta_tw, R, alpha, vi, omega, V)
+        T_calc = _thrust_single_rotor(rho, A, SR, a, theta_0, theta_tw, R, alpha, vi, omega, V)
         return T - T_calc
 
     res = scipy.optimize.least_squares(f, x0=5e3, bounds=(0, np.inf))
-    T_per_blade = res.x[0]
-    return T_per_blade * n_blades * n_rotors
+    T_per_rotor = res.x[0]
+    return T_per_rotor * n_rotors
 
 
 def solve_thrust_forward_flight(V, alpha):
@@ -94,19 +100,20 @@ def solve_thrust_forward_flight(V, alpha):
         alpha: angle of attack [rad]
 
     Returns:
-        Thrust [N]
+        Total thrust of all rotors [N]
     """
 
     def f(T):
         A = area(R)
         SR = solidity_ratio(chord, R)
         vi = velocity_induced_forward_flight(T, rho, A, V)
-        T_calc = _thrust_single_blade(rho, A, SR, a, theta_0, theta_tw, R, alpha, vi, omega, V)
+        T_calc = _thrust_single_rotor(rho, A, SR, a, theta_0, theta_tw, R, alpha, vi, omega, V)
         return T - T_calc
 
-    res = scipy.optimize.least_squares(f, x0=5e3, bounds=(1, np.inf))
-    T_per_blade = res.x[0]
-    return T_per_blade * n_blades * n_rotors
+    res = scipy.optimize.least_squares(f, x0=5e3, bounds=(0, np.inf))
+    T_per_rotor = res.x[0]
+    return T_per_rotor * n_rotors
+
 
 def solve_thrust_vertical_climb(V_c):
     """
@@ -117,23 +124,22 @@ def solve_thrust_vertical_climb(V_c):
         alpha: angle of attack [rad]
 
     Returns:
-        Thrust [N]
+        Total thrust of all rotors [N]
     """
-    alpha = np.pi/2
-    V = 0 #V_c
+    alpha = np.pi / 2
+    V = V_c
+
     def f(T):
         A = area(R)
         SR = solidity_ratio(chord, R)
         vi = velocity_induced_vertical_climb(T, rho, A, V)
-        T_calc = _thrust_single_blade(rho, A, SR, a, theta_0, theta_tw, R, alpha, vi, omega, V_c)
-        print('---------------')
-        print(T)
-        print(T_calc)
+        T_calc = _thrust_single_rotor(rho, A, SR, a, theta_0, theta_tw, R, alpha, vi, omega, V)
         return T - T_calc
 
-    res = scipy.optimize.least_squares(f, x0=12, bounds=(-1000, np.inf))
-    T_per_blade = res.x[0]
-    return T_per_blade * n_blades * n_rotors
+    res = scipy.optimize.least_squares(f, x0=5e3, bounds=(0, np.inf))
+    T_per_rotor = res.x[0]
+    return T_per_rotor * n_rotors
+
 
 def solve_rotor_torque_hover():
     V = 0
@@ -145,17 +151,18 @@ def solve_rotor_torque_hover():
     torquecalc = rotortorque(rho, A, SR, a, theta_0, theta_tw, R, alpha, vi, omega, V)
     return torquecalc
 
+
 def solve_rotor_torque_forward_flight(V, alpha):
     """
-        Calculate rotor thrust in forward flight.
+    Calculate rotor thrust in forward flight.
 
-        Args:
-            V: forward velocity [m/s]
-            alpha: angle of attack [rad]
+    Args:
+        V: forward velocity [m/s]
+        alpha: angle of attack [rad]
 
-        Returns:
-            Thrust [N]
-        """
+    Returns:
+        Total thrust of all rotors [N]
+    """
     A = area(R)
     SR = solidity_ratio(chord, R)
     T = solve_thrust_forward_flight(V, alpha)
@@ -163,17 +170,8 @@ def solve_rotor_torque_forward_flight(V, alpha):
     torquecalc = rotortorque(rho, A, SR, a, theta_0, theta_tw, R, alpha, vi, omega, V)
     return torquecalc
 
+
 def solve_rotor_torque_vertical_climb(V_c):
-    """
-        Calculate rotor thrust in forward flight.
-
-        Args:
-            V: forward velocity [m/s]
-            alpha: angle of attack [rad]
-
-        Returns:
-            Thrust [N]
-        """
     alpha = np.pi / 2
     V = V_c
     A = area(R)
@@ -182,6 +180,7 @@ def solve_rotor_torque_vertical_climb(V_c):
     vi = velocity_induced_vertical_climb(T, rho, A, V)
     torquecalc = rotortorque(rho, A, SR, a, theta_0, theta_tw, R, alpha, vi, omega, V)
     return torquecalc
+
 
 def hubforce():
     h1 = (1 / 4) * rho * A * SR * Cd * R
@@ -231,18 +230,18 @@ if __name__ == "__main__":
     angle_of_attack = -0.05
     climb_speed = 100
     print("forces:")
-    #print(solve_thrust_hover())
-    #print(solve_thrust_forward_flight(speed, angle_of_attack))
-    print(solve_thrust_vertical_climb(climb_speed))
+    # print(solve_thrust_hover())
+    # print(solve_thrust_forward_flight(speed, angle_of_attack))
+    # print(solve_thrust_vertical_climb(20))
     print("torques")
-    #print(solve_rotor_torque_hover())
-    #print(solve_rotor_torque_forward_flight(speed, angle_of_attack))
-    #print(solve_rotor_torque_vertical_climb(climb_speed))
+    # print(solve_rotor_torque_hover())
+    # print(solve_rotor_torque_forward_flight(speed, angle_of_attack))
+    # print(solve_rotor_torque_vertical_climb(climb_speed))
 
-    # plot_radius_rpm_range()
+    plot_radius_rpm_range()
 
 
-'''
+"""
     alpha = np.arange(-60, 60, 0.1)
     thrust = []
     for i in alpha:
@@ -250,4 +249,4 @@ if __name__ == "__main__":
 
     plt.plot(alpha, thrust)
     plt.show()
-'''
+"""
