ad1e0841867243ebfaf4f22c6663cf5d8a7f0b45
jalonso <63980841+javialonso05@users.noreply.github.com>
2023-05-11 15:25:31 +0200

    Made area and thrust into a function that can be exportable.

--- dse/preliminary/cruise_sizing.py
+++ dse/preliminary/cruise_sizing.py
@@ -2,44 +2,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-MTOM = 3000
-g_mars = 3.71
-max_thrust = 1.1 * MTOM * g_mars
-
-# Assumptions
-# 2D wing
-# Fly at AoA for Cl_max
-# Thrust is constant
+def area_and_thrust(thrust_deflection, cl, cd, MTOM):
+    A = np.array([[cl * q, np.sin(np.radians(thrust_deflection))],
+                  [-cd * q, np.cos(np.radians(thrust_deflection))]])
+    B = np.array([MTOM * g_mars, 0])
+    S, T = np.linalg.solve(A, B)
+    return S, T
 
 
-# OPTn airfoil data - AOA = 8 deg
-cl = 1.5
-cd = 0.11
+if __name__ == '__main__':
+    MTOM = 3000
+    g_mars = 3.71
+    max_thrust = 1.1 * MTOM * g_mars
 
-# Obtain data for S and gamma_e
-fig, (ax1, ax2) = plt.subplots(2, 1, sharex='all')
-fig.subplots_adjust(hspace=0)
+    # Assumptions
+    # 2D wing
+    # Fly at AoA for Cl_max
+    # Thrust is constant
 
-rho = 0.01
-for V in [112, 0.7*220]:
-    q = 0.5*rho*V**2
-    results_list = list()
-    for y in range(0, 91):
-        A = np.array([[cl*q, np.sin(np.radians(y))],
-                      [-cd*q, np.cos(np.radians(y))]])
-        B = np.array([MTOM*g_mars, 0])
-        S, T = np.linalg.solve(A, B)
-        results_list.append([y, S, T])
 
-    results_arr = np.array(results_list).T
+    # OPTn airfoil data - AOA = 8 deg
+    cl = 1.5
+    cd = 0.11
 
-    ax1.plot(results_arr[0], results_arr[1], label=f'V = {round(V, 1)}')
-    ax2.plot(results_arr[0], results_arr[2])
+    # Obtain data for S and gamma_e
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex='all')
+    fig.subplots_adjust(hspace=0)
 
-ax1.set(ylabel=r'Wing area [m$^2$]')
-ax1.legend()
-ax1.grid()
-ax2.plot([results_arr[0][0], results_arr[0][-1]], [max_thrust, max_thrust])
-ax2.set(xlabel='Thrust deflection [deg]', ylabel='Thrust [N]')
-ax2.grid()
-plt.show()
+    rho = 0.01
+    for V in [112, 0.7*220]:
+        q = 0.5*rho*V**2
+        results_list = list()
+        for y in range(0, 91):
+            S, T = area_and_thrust(y, cl, cd, MTOM)
+            results_list.append([y, S, T])
+
+        results_arr = np.array(results_list).T
+
+        ax1.plot(results_arr[0], results_arr[1], label=f'V = {round(V, 1)}')
+        ax2.plot(results_arr[0], results_arr[2])
+
+    ax1.set(ylabel=r'Wing area [m$^2$]')
+    ax1.legend()
+    ax1.grid()
+    ax2.plot([results_arr[0][0], results_arr[0][-1]], [max_thrust, max_thrust])
+    ax2.set(xlabel='Thrust deflection [deg]', ylabel='Thrust [N]')
+    ax2.grid()
+    plt.savefig('S&T-vs-gamma.png')
+    plt.show()
