1e2902dd7f6704168984e79115ed0fd7ed079fcb
jalonso <63980841+javialonso05@users.noreply.github.com>
2023-05-12 13:44:11 +0200

    Formatted RotorEngineSizing.py

--- dse/preliminary/RotorEngineSizing.py
+++ dse/preliminary/RotorEngineSizing.py
@@ -1,81 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#Given Values
+from tiltrotormain import constants
 from scipy import integrate
 from scipy.interpolate import InterpolatedUnivariateSpline
 from scipy.optimize import curve_fit
 from cruise_sizing import max_tipSpeed
+from power_sizing import power
 
-'''
-def RadiusfromMass(M):
-    N_rotor=4
-    T_init = 1.1*M * 3.721 / N_rotor
-    b=6 #Number of blades per rotor
-    a=6 #Lift Curve slope [1/rad]
-    R=1
-    cd=0.06
-    theta_tip = 10 *np.pi/180
-    T=0
 
-    def func(x):
-        return theta_tip*R*x - x**2 * np.arctan(v1/(omega*x))
-
-    def func2(x):
-        return x*c*rho/2*omega**2 * (x**2*a*(theta_tip/(x/R)-np.arctan(v1/(omega*x)))*np.arctan(v1/(omega*x))+cd*x**2)
-
-    while T<T_init:
-        R=R+0.1
-        c = R / 25  # Chord of each rotor
-        A = np.pi * R ** 2  # Area of the rotor
-        omega = 200 / R
-        v1 = np.sqrt(T_init / (2 * rho * A))
-        #T=0.88 * b * rho * omega**2 * a *c/2*(theta_tip * R**3 /2-(v1 * (np.log(v1**2 + (omega*R)**2)-np.log(v1**2))/(2*omega)))
-        integral, accuracy = integrate.quad(func, 0, R)
-        T = 0.88 * b/2*rho*omega**2 *a*c * integral
-        Q, Q_accuracy = integrate.quad(func2, 0, R)
-        Q = Q*0.88*b
-    sigma = b*c/(np.pi*R)
-    Cq = Q/(rho*sigma*A*(omega*R)**2*R)
-    Ct = T/(rho*sigma*A*(omega*R)**2)
-    FM = np.sqrt(sigma/2) * Ct**(3/2)*Cq
-    Hp = T*v1*N_rotor*0.00134
-    return R, accuracy, Hp, sigma, Ct, Cq, FM
-'''
 def RadiusMassElementMomentum(M, N_rotors, N_blades, coaxial, V_tip, print_results=False):
-    gm=3.721
-    T_min = 1.1*M*gm /N_rotors
-    b=N_blades
-    T=0
-    R=1
-    while T<T_min:
-        R+=0.1
-        c=R/20
-        sigma = b*c/(np.pi*R)
-        theta_tip = 6*np.pi/180
-        x0 = c/R
-        V_m = 220
-        v_tip = V_tip
-        omega = v_tip/R
-        rho = 0.01
+    # Rename constants
+    gm = constants['gravityMars']
+    rho = constants['airDensity']
+    V_m = constants['soundSpeed']
+
+    b = N_blades
+    v_tip = V_tip
+    T_min = 1.1 * M * gm /N_rotors
+
+    # Initial guesses
+    T = 0
+    R = 1
+    while T < T_min:
+        R += 0.1  # Increase the radius
+        c = R/20  # Update the chord
+
+        theta_tip = np.radians(6)  # Assumed angle
+        x0 = c/R  # Distance from the blade's base to the centre of rotation
+        omega = v_tip/R  # Angular velocity
+
         n_elements = 10
         a0 = 6
-        alpha0 = 2*np.pi/180
+        alpha0 = np.radians(2)
         A = np.pi*R**2
+
         r2R = np.arange(1, n_elements+1)/n_elements
         c2R = c/R
-        M_local = (r2R)*(omega*R/V_m)
+        M_local = (r2R)*(omega*R/V_m)  # Local Mach number (omega*R = V_tip which is constant)
         a = a0/(np.sqrt(1-M_local**2))
         Dtheta = theta_tip/r2R
         theta0 = -min(Dtheta-np.ones(n_elements,)*alpha0)+theta_tip
         theta = theta0+Dtheta-alpha0
         v12Omegar = a*b*c2R/(16*np.pi*r2R)*(-1+np.sqrt(1+(32*np.pi*theta*r2R)/(a*b*c2R)))
-        alpha = theta-np.arctan(v12Omegar)
+
         alpha = 6*np.pi/180 * np.ones(n_elements)
-        cl = a*alpha
-        cd = 0.025
+
         #S1223
         cl = 1.6
-        cd=0.05
+        cd = 0.05
         DctDr2R = b*r2R**2*c2R*cl/(2*np.pi)
 
         funCT = InterpolatedUnivariateSpline(r2R, DctDr2R)
@@ -95,41 +67,36 @@ def RadiusMassElementMomentum(M, N_rotors, N_blades, coaxial, V_tip, print_resul
 
         DCQ_I = 0.01*CQ_induced
 
-        DL = Ct*rho*(omega*R)**2
+        T = rho*A*(omega*R)**2*Ct
+        if coaxial:
+            T *= 0.88
 
-        Ct2sigma = Ct/(b*c/(np.pi*R))
-
-        Cq = (CQ_profile+CQ_induced+DCQ_I)*.93
-
-        T=rho*A*(omega*R)**2*Ct
-        if coaxial == True:
-            T*=0.88
-        power = rho*A*(omega*R)**3*Cq
-        power = T * np.sqrt(T / (2 * 0.01 * np.pi * R * R))
+        pow = power(T, R)
 
     #Rotor Weight:
     def NACA0012airfoilfunction(x):
-        return 5 * 0.12 * (0.2969* (x**0.5) - 0.1260 * x - 0.3516 * (x ** 2) + 0.2843 * (x ** 3) - 0.1036 * (x ** 4))
+        return 5 * 0.12 * (0.2969 * (x**0.5) - 0.1260 * x - 0.3516 * (x ** 2) + 0.2843 * (x ** 3) - 0.1036 * (x ** 4))
 
 
     Area = integrate.quad(NACA0012airfoilfunction, 0, 1)[0] * c
-    fillfactor=0.3
+    fillfactor = 0.3
     Rotor_mass = 1500*R*Area*fillfactor
 
     if print_results:
         print('Given the mass of '+str(M)+'kg, the following applied: \n')
         print('Rotor Radius: '+str(R)+'[m]')
-        print('Rotor Power: '+str(power)+'[W]')
-        print('Total Aircraft power: '+str(power*N_rotors)+'[W]')
+        print('Rotor Power: ' + str(pow) + '[W]')
+        print('Total Aircraft power: ' + str(pow * N_rotors) + '[W]')
         print('Weight per rotor with '+str(b)+' blades is '+ str(Rotor_mass)+'[kg]')
         print('Total Rotor mass for entire aircraft: '+str(Rotor_mass*N_rotors))
 
-    return R, T, power/745.7, power*N_rotors, Rotor_mass*N_rotors
+    return R, T, pow / 745.7, pow * N_rotors, Rotor_mass * N_rotors
+
 
 if __name__ == '__main__':
-    gm = 3.721
+    gm = constants['gravityMars']
+    rho = constants['airDensity']  # kg/m3
     MTOW = 3000 * gm
-    rho = 0.01  # kg/m3
 
     # Blade Element Method with ideal twist
     N_rotor = 4
--- dse/preliminary/tiltrotormain.py
+++ dse/preliminary/tiltrotormain.py
@@ -1,28 +1,36 @@
 from RotorEngineSizing import RadiusMassElementMomentum
 from AircraftEstimating import Class2Weight
-#Sizing of Rotor:
+
+
+# Constant
+constants = {
+    'gravityMars': 3.71,
+    'airDensity': 0.01,
+    'soundSpeed': 220
+}
+
+# Sizing of Rotor:
 margin = 0.1
-Mass_thrust = 3000*(1+margin) #kg
-Mass_design = 3000 #kg
-Mass_payload = 400 #kg
-Mass_solar = 0 #kg
+Mass_thrust = 3000*(1+margin)  # kg
+Mass_design = 3000  # kg
+Mass_payload = 400  # kg
+Mass_solar = 0  # kg
 
 TotalRotors = 4
 coaxial = True
-TipSpeed = 160 # m/s
+TipSpeed = 160  # m/s
 BladePerRotor = 6
 
 N_ult = 4.4
 AR = 20
 wingbraced = True
-V_cr = 112 #m/s
-E_density = 333.33 #W/kg
-P_density_TO = 700 #Wh/kg Power Density of the devices used for take-off
-E_density_TO =  300 #W/kg
-Volume_bat = 450 # Wh/L
+V_cr = 112  # m/s
+E_density = 333.33  # W/kg
+P_density_TO = 700  # Wh/kg Power Density of the devices used for take-off
+E_density_TO =  300  # W/kg
+Volume_bat = 450  # Wh/L
 
-
-
+# Calculate parameters
 R, T, Horsepower, Power, mass_rotors = RadiusMassElementMomentum(Mass_thrust, TotalRotors, BladePerRotor, coaxial, TipSpeed)
 Class2Weight(R, mass_rotors, Mass_design, N_ult, AR, wingbraced, V_cr, E_density, P_density_TO, E_density_TO, Mass_payload, Mass_solar)
 
