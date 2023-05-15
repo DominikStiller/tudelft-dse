3755940f8364b1d9437770681321fe17c735d905
Dominik Stiller <domi.stiller@gmail.com>
2023-05-15 23:09:31 +0200

    Improve multicopter payload-range diagram

--- dse/preliminary/multicopter/payload_range.py
+++ dse/preliminary/multicopter/payload_range.py
@@ -6,35 +6,47 @@ if __name__ == "__main__":
     km_per_kg = (400 / 146) / 1.3
     mtom = 2700  # kg
     oem = 1747  # kg
-    max_payload = 350  # kg
     takeoff_plus_land = 36.4 * 2  # kg
+
+    def payload2fuel(payload):
+        return mtom - oem - payload
+
+    def fuel2payload(fuel):
+        return mtom - oem - takeoff_plus_land - fuel
+
+    max_payload = mtom - oem - takeoff_plus_land
+    print(f"{max_payload=}")
+
+    max_payload_at_1000km = mtom - oem - takeoff_plus_land - 1000 / km_per_kg
+    print(f"{max_payload_at_1000km=}")
+
+    range_at_350kg_payload = km_per_kg * (mtom - oem - takeoff_plus_land - 350)
+    print(f"{range_at_350kg_payload=}")
 
     max_fuel_at_max_payload = mtom - oem - takeoff_plus_land - max_payload
     max_range_at_max_payload = max_fuel_at_max_payload * km_per_kg
-    print(max_range_at_max_payload)
+    print(f"{max_range_at_max_payload}")
 
-    max_fuel_at_no_payload = mtom - oem
+    max_fuel_at_no_payload = mtom - oem - takeoff_plus_land
     max_range_at_no_payload = max_fuel_at_no_payload * km_per_kg
-    print(max_range_at_no_payload)
-
-    # # For flying wing
-    # max_payload = 350
-    # max_range_at_max_payload = 1000
-    # max_range_at_no_payload = 6241.043
+    print(f"{max_range_at_no_payload}")
 
     fig, ax = plt.subplots(figsize=(10, 3))
+    secax = ax.secondary_yaxis("right", functions=(payload2fuel, fuel2payload))
+    secax.set_ylabel("Fuel mass [kg]")
 
     ax.plot(
         [0, max_range_at_max_payload, max_range_at_no_payload],
         [max_payload, max_payload, 0],
         c="black",
     )
-    ax.vlines(max_range_at_max_payload, 0, max_payload, color="black", ls=":")
+    ax.vlines(1000, 0, max_payload_at_1000km, color="black", ls=":")
+    ax.hlines(350, 0, range_at_350kg_payload, color="black", ls=":")
     ax.set_xlabel("Range [km]")
     ax.set_ylabel("Payload mass [kg]")
     ax.set_xlim([0, max_range_at_no_payload * 1.05])
     ax.set_ylim([0, max_payload * 1.05])
 
     format_plot()
-    save_plot(".", "multicopter_payload_range")
+    save_plot(".", "multicopter_payload_range", type="png")
     plt.show()
