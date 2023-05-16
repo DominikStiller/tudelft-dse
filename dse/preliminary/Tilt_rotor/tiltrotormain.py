f0d0e8f838944f29796dd870894806613f14b8b1
jalonso <63980841+javialonso05@users.noreply.github.com>
2023-05-16 15:54:32 +0200

    Updated drag calculation

--- dse/preliminary/Tilt_rotor/tiltrotormain.py
+++ dse/preliminary/Tilt_rotor/tiltrotormain.py
@@ -4,7 +4,7 @@ from RotorEngineSizing import RadiusMassElementMomentum
 from structures import calculate_cg, assembly_volume
 from constants import const, aircraftParameters
 from power_sizing import size_power_subsystem
-from cruise_sizing import area_and_thrust
+from cruise_sizing import area
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -16,46 +16,32 @@ def design(iterate=False):
     while diff > 0.01 and Range >= 1000:
         const['airDensity'] = 0.01
         Mass_design = aircraftParameters['totalMass']
-        tipSpeed = -0.88 * const['cruiseSpeed'] + 268.87
+
+        takeOffTipSpeed = 0.92 * const['soundSpeed']
         Mass_thrust = const['maxMass'] * (1 + const['margin'])  # kg
         aircraftParameters['rotorRadius'], takeOffThrustPerEngine, aircraftParameters['horsepowerPerEngine'], \
         aircraftParameters['totalPower'], aircraftParameters['rotorMass'] = \
             RadiusMassElementMomentum(M=Mass_thrust, N_rotors=aircraftParameters['totalRotors'],
                                       N_blades=aircraftParameters['bladesPerRotor'],
-                                      coaxial=aircraftParameters['coaxial'], V_tip=tipSpeed, print_results=True)
+                                      coaxial=aircraftParameters['coaxial'], V_tip=takeOffTipSpeed, print_results=True)
 
         # Calculate wing area
-        aircraftParameters['wingArea'], aircraftParameters['cruiseThrust'] = \
-            area_and_thrust(0, const['cl'], const['cd'], Mass_design,
-                            0.5 * const['airDensity'] * const['cruiseSpeed'] ** 2)
+        area(const['cl'], Mass_design, 0.5 * const['airDensity'] * const['cruiseSpeed'] ** 2)
 
-        aircraftParameters['wingspan'] = 3 * aircraftParameters['rotorRadius']
-        aircraftParameters['chord'] = aircraftParameters['wingArea'] / aircraftParameters['wingspan']
-        aircraftParameters['AR'] = aircraftParameters['wingspan'] / aircraftParameters['chord']
-
-        if aircraftParameters['wingArea'] / aircraftParameters['wingspan'] < aircraftParameters['ARmin']:
-            print(f'Wing span is too large for the required AR')
-            aircraftParameters['wingspan'] = np.sqrt(aircraftParameters['wingArea'] * aircraftParameters['ARmin'])
-            aircraftParameters['chord'] = aircraftParameters['wingArea'] / aircraftParameters['wingspan']
-
-        print(f'Wing area = {aircraftParameters["wingArea"]}[m2]')
-        print(f'Wingspan = {aircraftParameters["wingspan"]}[m]')
-        print(f'Chord = {aircraftParameters["chord"]}[m]')
-        print(f'AR = {aircraftParameters["wingspan"] / aircraftParameters["chord"]}')
+        cd0 = DragEstimation(aircraftParameters['wingArea'], const['cruiseSpeed'], const['visc_cr'])
+        aircraftParameters['cruiseThrust'] = (cd0 + const['cl']**2/(np.pi*aircraftParameters['AR']*const['oswald'])) * \
+            0.5 * const['airDensity'] * const['cruiseSpeed']**2 * aircraftParameters['wingArea']
+        print(f'Cruise drag = {aircraftParameters["cruiseThrust"]}')
 
         const['airDensity'] = 0.01
 
         # Size batteries for cruise and solar panels
-        aircraftParameters['cruiseThrust'] += DragEstimation(R=aircraftParameters['rotorRadius'],
-                                                         Swing=aircraftParameters['wingArea'], t2c=const['t/c'],
-                                                         Vcr=const['cruiseSpeed'], visc_cr=const['visc_cr'],
-                                                         AR=aircraftParameters['AR'])
         print(f'Total Drag on the aircraft: {aircraftParameters["cruiseThrust"]}[N]')
         aircraftParameters['batteryMass'], aircraftParameters['panelMass'], powerSurplus = \
             size_power_subsystem(aircraftParameters['rotorRadius'], takeOffThrustPerEngine,
                                  aircraftParameters['cruiseThrust'],
                                  const['designRange'] / const['cruiseSpeed'] + const['takeOffTime'],
-                                 const['takeOffTime'], aircraftParameters['wingArea'], plot=True)
+                                 const['takeOffTime'], aircraftParameters['wingArea'], plot=False)
 
         # Calculate weights
         Range, aircraftParameters['wingMass'], aircraftParameters['tailMass'], aircraftParameters['bodyMass'] = \
@@ -122,6 +108,6 @@ if __name__ == '__main__':
 
     m, dim, acParams = design(iterate=True)
 
-    # plt.legend(loc='best')
-    # plt.savefig('../Figures/Payload-Range.pdf')
-    # plt.show()
+    plt.legend(loc='best')
+    plt.savefig('../Figures/Payload-Range.pdf')
+    plt.show()
