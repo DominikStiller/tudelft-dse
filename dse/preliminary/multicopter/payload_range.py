1da2b829f093b41279b55300b0d325e837c8c8ba
Dominik Stiller <domi.stiller@gmail.com>
2023-05-16 11:52:31 +0200

    Improve multicopter payload-range diagram

--- dse/preliminary/multicopter/payload_range.py
+++ dse/preliminary/multicopter/payload_range.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 from dse.plotting import format_plot, save_plot
 
 if __name__ == "__main__":
-    km_per_kg = (400 / 146) / 1.3
+    kg_per_h = 47
+    km_per_kg = (400 / kg_per_h) / 1.3
     mtom = 2700  # kg
     oem = 1747  # kg
-    takeoff_plus_land = 36.4 * 2  # kg
+    takeoff_plus_land = 72.2  # kg
 
     def payload2fuel(payload):
         return mtom - oem - payload
@@ -20,16 +21,16 @@ if __name__ == "__main__":
     max_payload_at_1000km = mtom - oem - takeoff_plus_land - 1000 / km_per_kg
     print(f"{max_payload_at_1000km=}")
 
-    range_at_350kg_payload = km_per_kg * (mtom - oem - takeoff_plus_land - 350)
-    print(f"{range_at_350kg_payload=}")
+    max_range_at_350kg_payload = km_per_kg * (mtom - oem - takeoff_plus_land - 350)
+    print(f"{max_range_at_350kg_payload=}")
 
     max_fuel_at_max_payload = mtom - oem - takeoff_plus_land - max_payload
     max_range_at_max_payload = max_fuel_at_max_payload * km_per_kg
-    print(f"{max_range_at_max_payload}")
+    print(f"{max_range_at_max_payload=}")
 
     max_fuel_at_no_payload = mtom - oem - takeoff_plus_land
     max_range_at_no_payload = max_fuel_at_no_payload * km_per_kg
-    print(f"{max_range_at_no_payload}")
+    print(f"{max_range_at_no_payload=}")
 
     fig, ax = plt.subplots(figsize=(10, 3))
     secax = ax.secondary_yaxis("right", functions=(payload2fuel, fuel2payload))
@@ -41,7 +42,7 @@ if __name__ == "__main__":
         c="black",
     )
     ax.vlines(1000, 0, max_payload_at_1000km, color="black", ls=":")
-    ax.hlines(350, 0, range_at_350kg_payload, color="black", ls=":")
+    ax.hlines(350, 0, max_range_at_350kg_payload, color="black", ls=":")
     ax.set_xlabel("Range [km]")
     ax.set_ylabel("Payload mass [kg]")
     ax.set_xlim([0, max_range_at_no_payload * 1.05])
