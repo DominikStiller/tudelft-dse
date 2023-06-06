88081a1aeb817abe55300841707c55beac8cfe8a
Dominik Stiller <domi.stiller@gmail.com>
2023-06-06 11:50:08 +0200

    Add more information to link budget

--- dse/detailed/communication/linkbudget.py
+++ dse/detailed/communication/linkbudget.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import scipy.optimize
 from tabulate import tabulate, SEPARATING_LINE
 
 from dse.detailed.communication import from_db, to_db, k
-from dse.detailed.communication.waves import calculate_free_space_loss, wavelength
+from dse.detailed.communication.waves import (
+    calculate_free_space_loss,
+    wavelength,
+    calculate_travel_time,
+)
 
 
 def calculate_line_temp(line_loss: float, T0: float = 290):
@@ -21,12 +26,25 @@ def calculate_receiver_temp(line_loss: float, noise_figure: float, T0: float = 2
     return T0 * (noise_figure - 1) / line_loss
 
 
+def calculate_system_noise_temp(line_loss: float, noise_figure: float, temperature_antenna: float):
+    temperature_line = calculate_line_temp(line_loss)
+    temperature_receiver = calculate_receiver_temp(line_loss, noise_figure)
+    return temperature_antenna + temperature_line + temperature_receiver
+
+
 def calculate_uplink_data_rate():
-    data_rate_voice = 128e3  # CCSDS 766.2-B-1, 4.2.1.5
-    data_rate_biomed = 33e3
-    data_rate_telemetry = 0.8e3
-    factor = 1.5
+    data_rate_voice = 8e3  # CCSDS 766.2-B-1, 4.5.1, use G.729 with 8 kb/s
+    data_rate_biomed = 0.2e3  # 50 * 4 * 1
+    data_rate_telemetry = 8e3  # 200 * 4 * 10
+    factor = 2
     return factor * (data_rate_voice + data_rate_biomed + data_rate_telemetry)
+
+
+def calculate_downlink_data_rate():
+    data_rate_voice = 8e3  # CCSDS 766.2-B-1, 4.5.1, use G.729 with 8 kb/s
+    data_rate_mission = 10e3
+    factor = 2
+    return factor * (data_rate_voice + data_rate_mission)
 
 
 def required_snr(bit_error_rate: float, coding: str):
@@ -42,11 +60,23 @@ def required_snr(bit_error_rate: float, coding: str):
     elif coding == "conv-7-1/2":
         # From CCSDS 130.1-G-3, Figure 3-5
         return 4.8
+    elif coding == "ldpc-1/2":
+        # From CCSDS 130.1-G-3, Figure 3-5
+        return 1
     else:
         raise "Unsupported coding scheme"
 
 
-class LinkBudget:
+def coding_datarate_factor(coding: str):
+    if coding == "uncoded":
+        return 1
+    elif "1/2" in coding:
+        return 1 / 2
+    else:
+        raise "Unsupported coding scheme"
+
+
+class Link:
     def __init__(
         self,
         frequency: float,
@@ -59,12 +89,15 @@ class LinkBudget:
         rx_pointing_loss: float,
         rx_gain: float,
         rx_loss: float,
-        line_loss: float,
-        noise_figure: float,
-        temperature_antenna: float,
         data_rate: float,
         bit_error_rate: float,
         coding: str,
+        line_loss: float = None,
+        noise_figure: float = None,
+        temperature_antenna: float = None,
+        temperature_system_noise: float = None,
+        margin_target: float = 3,
+        latency_factor: float = 1,
     ):
         """
         Initialize class.
@@ -72,20 +105,23 @@ class LinkBudget:
         Args:
             frequency:
             tx_power:
-            tx_loss:
+            tx_loss: [dB]
             tx_gain: [dBi]
             tx_pointing_loss: negative [dBi]
             tx_rx_distance:
             loss_environment: negative [dBi]
             rx_pointing_loss: negative [dBi]
             rx_gain: [dBi]
-            rx_loss: [-]
-            line_loss: [-]
-            noise_figure: [-]
-            temperature_antenna:
+            rx_loss: [dB]
             data_rate:
             bit_error_rate:
             coding:
+            line_loss: [-]
+            noise_figure: [-]
+            temperature_antenna:
+            temperature_system_noise:
+            margin_target: [dB]
+            latency_factor:
         """
         self.frequency = frequency
         self.tx_power = tx_power
@@ -97,52 +133,48 @@ class LinkBudget:
         self.rx_pointing_loss = rx_pointing_loss
         self.rx_gain = rx_gain
         self.rx_loss = rx_loss
-        self.line_loss = line_loss
-        self.noise_figure = noise_figure
-        self.temperature_antenna = temperature_antenna
         self.data_rate = data_rate
         self.bit_error_rate = bit_error_rate
         self.coding = coding
+        self.margin_target = margin_target
+        self.latency_factor = latency_factor
+
+        if temperature_system_noise is not None:
+            assert line_loss is None
+            assert noise_figure is None
+            assert temperature_antenna is None
+            self.temperature_system_noise = temperature_system_noise
+        else:
+            self.line_loss = line_loss
+            self.noise_figure = noise_figure
+            self.temperature_antenna = temperature_antenna
+            self.temperature_system_noise = calculate_system_noise_temp(
+                line_loss, noise_figure, temperature_antenna
+            )
 
         self.budget = None
-        self.temperature_system_noise = None
         self.snr = None
+        self.max_data_rate = None
 
-        self._calculate_budget()
+        self._calculate_max_data_rate()
+        self._calculate_budget(self.data_rate)
 
-    def _calculate_budget(self):
+    def _calculate_budget(self, data_rate):
         loss_free_space = to_db(calculate_free_space_loss(self.tx_rx_distance, self.frequency))
+        required_datarate = data_rate / coding_datarate_factor(self.coding)
 
-        temperature_line = calculate_line_temp(self.line_loss)
-        temperature_receiver = calculate_receiver_temp(self.line_loss, self.noise_figure)
-        self.temperature_system_noise = (
-            self.temperature_antenna + temperature_line + temperature_receiver
-        )
-        # self.temperature_system_noise = 500
-
-        self.budget = [
-            to_db(self.tx_power),
-            to_db(self.tx_loss),
-            self.tx_gain,
-            self.tx_pointing_loss,
-            loss_free_space,
-            self.loss_environment,
-            self.rx_pointing_loss,
-            self.rx_gain,
-            self.rx_loss,
-        ]
         self.budget = pd.DataFrame(
             [
                 ("Tx power", to_db(self.tx_power), "dBW"),
-                ("Tx loss factor", to_db(self.tx_loss), "dB"),
-                ("Tx antenna pointing loss", self.tx_pointing_loss, "dB"),
+                ("Tx line loss", self.tx_loss, "dB"),
                 ("Tx antenna gain", self.tx_gain, "dBi"),
+                ("Tx antenna pointing loss", self.tx_pointing_loss, "dB"),
                 ("Free space loss", loss_free_space, "dB"),
                 ("Environment loss", self.loss_environment, "dB"),
                 ("Rx antenna gain", self.rx_gain, "dBi"),
                 ("Rx antenna pointing loss", self.rx_pointing_loss, "dB"),
-                ("Rx loss factor", to_db(self.rx_loss), "dB"),
-                ("Required data rate", to_db(1 / self.data_rate), "dB(bit/s)"),
+                ("Rx line loss", self.rx_loss, "dB"),
+                ("Required data rate", to_db(1 / required_datarate), "dB(bit/s)"),
                 ("Boltzmann constant", to_db(1 / k), "dB(J/K)"),
                 ("System noise temperature", to_db(1 / self.temperature_system_noise), "dB(K)"),
             ],
@@ -161,14 +193,32 @@ class LinkBudget:
             columns=["Component", "Value [dB]", "Unit"],
         )
 
+        return snr_margin
+
+    def _calculate_max_data_rate(self):
+        def fn(data_rate):
+            return self._calculate_budget(data_rate) - self.margin_target
+
+        res = scipy.optimize.least_squares(fn, self.data_rate)
+        self.max_data_rate = res.x[0]
+
     def print_configuration(self):
         print(f"Transmitter power: {self.tx_power:.0f} W")
         print(f"Frequency: {self.frequency / 1e6:.1f} MHz")
+        print(f"System noise temperature: {self.temperature_system_noise:.0f} K")
         print(f"Wavelength: {wavelength(self.frequency):.3g} m")
         print(f"1/4 Wavelength: {wavelength(self.frequency)/4:.3g} m")
 
         g_over_t = self.rx_gain - to_db(self.temperature_system_noise)
         print(f"G/T: {g_over_t:.1f} dB/K")
+
+        print(f"Nominal data rate: {self.data_rate/1e3:.1f} kb/s")
+        print(
+            f"Maximum data rate (with {self.margin_target:.0f} dB margin): {self.max_data_rate/1e3:.1f} kb/s"
+        )
+
+        latency = self.latency_factor * calculate_travel_time(self.tx_rx_distance)
+        print(f"Latency: {latency * 1e3:.0f} ms")
 
     def print_table(self):
         budget = list(self.budget.itertuples(index=False, name=None))
@@ -177,54 +227,104 @@ class LinkBudget:
 
 
 if __name__ == "__main__":
-    aircraft_gain_uhf = 6
+    relay_gain_uhf = 0
+    aircraft_gain_uhf = 4
     aircraft_gain_hf = 0
+    base_gain_hf = 0
+
+    tx_loss = -1
+    rx_loss = -2
+
+    relay_tx_rx_distance = 8000e3
+    skywave_tx_rx_distance = 933e3
 
     print("UPLINK (RELAY)")
-    budget_uplink_relay = LinkBudget(
+    budget_uplink_relay = Link(
         frequency=440e6,
-        tx_power=100,
-        tx_loss=0.7,
+        tx_power=55,
+        tx_loss=tx_loss,
         tx_gain=aircraft_gain_uhf,
         tx_pointing_loss=-2,
-        tx_rx_distance=8000e3,
+        tx_rx_distance=relay_tx_rx_distance,
         loss_environment=-0.5,
         rx_pointing_loss=-2,
-        rx_gain=0,
-        rx_loss=0.7,
-        line_loss=from_db(-1),
-        noise_figure=from_db(4.9),
-        temperature_antenna=155,
+        rx_gain=relay_gain_uhf,
+        rx_loss=rx_loss,
         data_rate=calculate_uplink_data_rate(),
         bit_error_rate=1e-6,
-        coding="conv-7-1/2",
+        coding="ldpc-1/2",
+        temperature_system_noise=500,
+        latency_factor=2,
     )
     budget_uplink_relay.print_configuration()
     budget_uplink_relay.print_table()
 
     print()
-    print("UPLINK (SKYWAVE)")
-    budget_uplink_skywave = LinkBudget(
-        frequency=10e6,
-        tx_power=20,
-        tx_loss=0.7,
-        tx_gain=aircraft_gain_hf,
+    print("DOWNLINK (RELAY)")
+    budget_downlink_relay = Link(
+        frequency=420e6,
+        tx_power=100,
+        tx_loss=tx_loss,
+        tx_gain=relay_gain_uhf,
         tx_pointing_loss=-2,
-        tx_rx_distance=933e3,
+        tx_rx_distance=relay_tx_rx_distance,
         loss_environment=-0.5,
         rx_pointing_loss=-2,
-        rx_gain=0,
-        rx_loss=0.7,
+        rx_gain=aircraft_gain_uhf,
+        rx_loss=rx_loss,
+        line_loss=from_db(-1),
+        noise_figure=from_db(4.9),
+        temperature_antenna=54,
+        data_rate=calculate_downlink_data_rate(),
+        bit_error_rate=1e-6,
+        coding="ldpc-1/2",
+        latency_factor=2,
+    )
+    budget_downlink_relay.print_configuration()
+    budget_downlink_relay.print_table()
+
+    print()
+    print("UPLINK (SKYWAVE)")
+    budget_uplink_skywave = Link(
+        frequency=4e6,
+        tx_power=1,
+        tx_loss=tx_loss,
+        tx_gain=aircraft_gain_hf,
+        tx_pointing_loss=-2,
+        tx_rx_distance=skywave_tx_rx_distance,
+        loss_environment=2 * -0.5,
+        rx_pointing_loss=-2,
+        rx_gain=base_gain_hf,
+        rx_loss=rx_loss,
         line_loss=from_db(-1),
         noise_figure=from_db(4.9),
         temperature_antenna=155,
         data_rate=calculate_uplink_data_rate(),
         bit_error_rate=1e-6,
-        coding="conv-7-1/2",
+        coding="ldpc-1/2",
     )
-    # TODO check if losses are correct
     budget_uplink_skywave.print_configuration()
     budget_uplink_skywave.print_table()
 
-    # TODO downlink
-    # TODO calculate max data rate
+    print()
+    print("DOWNLINK (SKYWAVE)")
+    budget_downlink_relay = Link(
+        frequency=4e6,
+        tx_power=1,
+        tx_loss=tx_loss,
+        tx_gain=base_gain_hf,
+        tx_pointing_loss=-2,
+        tx_rx_distance=skywave_tx_rx_distance,
+        loss_environment=2 * -0.5,
+        rx_pointing_loss=-2,
+        rx_gain=aircraft_gain_hf,
+        rx_loss=rx_loss,
+        line_loss=from_db(-1),
+        noise_figure=from_db(4.9),
+        temperature_antenna=54,
+        data_rate=calculate_downlink_data_rate(),
+        bit_error_rate=1e-6,
+        coding="ldpc-1/2",
+    )
+    budget_downlink_relay.print_configuration()
+    budget_downlink_relay.print_table()
--- dse/detailed/communication/waves.py
+++ dse/detailed/communication/waves.py
@@ -13,3 +13,8 @@ def calculate_free_space_loss(distance: float, frequency: float):
     # distance [m]
     # frequency [Hz]
     return (wavelength(frequency) / (4 * pi * distance)) ** 2
+
+
+def calculate_travel_time(distance: float):
+    # distance [m]
+    return distance / c
