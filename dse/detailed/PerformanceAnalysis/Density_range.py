a626536d97dc12d9e5ab2928657c3837ce3c691f
jalonso <63980841+javialonso05@users.noreply.github.com>
2023-06-21 14:37:13 +0200

    Small bug fixed.

--- dse/detailed/PerformanceAnalysis/Density_range.py
+++ dse/detailed/PerformanceAnalysis/Density_range.py
@@ -4,11 +4,12 @@ from plotting import format_plot, save_plot
 
 
 if __name__ == '__main__':
-    energy_available = np.array([[790.125], [924.875]]) * 1e3
-    flight_time = 1000/400  # hours
-    power_required = ...
-    max_range = energy_available / (flight_time * power_required)
-    density = np.arange(0.01, 0.02, np.shape(power_required)[1])
+    TO_energy = 59206.51
+    energy_available = np.array([[790.125], [924.875]]) * 1e3 - 2 * TO_energy
+    power_required = 4 * np.ones((2, 1)) * np.array([46589.11257, 103365.1217, 113701.6338, 124038.146, 134374.6582,
+                                                 144711.1703, 124648.1978, 132958.0776, 141267.9575, 149577.8373])
+    max_range = 400 * (energy_available / power_required)
+    density = np.linspace(0.01, 0.02, np.shape(power_required)[1])
 
     plt.figure(figsize=(9, 2.5))
     plt.plot(density, max_range.T)
