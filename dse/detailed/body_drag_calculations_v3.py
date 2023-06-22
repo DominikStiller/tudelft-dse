0d0c2e22740fd6dad9fea92da3febe9a9f128396
Dominik Stiller <domi.stiller@gmail.com>
2023-06-22 15:23:59 +0200

    Format body drag calculations

--- dse/detailed/body_drag_calculations_v3.py
+++ dse/detailed/body_drag_calculations_v3.py
@@ -2,16 +2,14 @@ import numpy as np
 
 
 class Fuselage:
-
     def __init__(self, cabin_width, cabin_height, fuselage_length, fuselage_area):
-
         # Useful characteristics
         self.width = cabin_width
         self.height = cabin_height
         self.length = fuselage_length
         self.area = fuselage_area
 
-        self.r_main = (self.width + self.height)/4
+        self.r_main = (self.width + self.height) / 4
         self.d_main = self.r_main * 2
 
         self.cd = self.calculate_cd()
@@ -41,11 +39,13 @@ class Fuselage:
     #  --------- Methods for calculating characteristics --------- #
 
     def calculate_cd(self):
-        """ Calculates cd [-] based on the fuselage characteristics """
+        """Calculates cd [-] based on the fuselage characteristics"""
         # Calculations in excel - based on Torenbeek
         perfect_cylinder_area = 2 * np.pi * self.r_main * (self.r_main + self.length)
-        CdS = 0.0031 * 1.15 * self.length * (self.height + self.width)  # (Torenbeek page 150) # This is Cd * S_factor. Not real S.
-        S_ratio = 0.5 * np.pi * self.length * (self.height + self.width)/2  # (Torenbeek page 150)
+        CdS = (
+            0.0031 * 1.15 * self.length * (self.height + self.width)
+        )  # (Torenbeek page 150) # This is Cd * S_factor. Not real S.
+        S_ratio = 0.5 * np.pi * self.length * (self.height + self.width) / 2  # (Torenbeek page 150)
 
         # The  aerodynamic format of the current shape surely contributes less to drag than a cylinder of equal area.
         # In order to account for this, this ratio is calculated and applied to the hypothetical fully cylindrical
@@ -56,48 +56,46 @@ class Fuselage:
         return Cd
 
     def drag_simulation(self, velocity, air_density):
-        """ Calculates fuselage drag [N] based on dynamic pressure. It takes velocity [m/s] and air density [kg/m^3] """
-        q = 0.5 * air_density * velocity ** 2
+        """Calculates fuselage drag [N] based on dynamic pressure. It takes velocity [m/s] and air density [kg/m^3]"""
+        q = 0.5 * air_density * velocity**2
         drag_force = self.cd * self.area * q
         return drag_force
-
 
     # --------- Static Methods --------- #
 
     @staticmethod
     def area_cylinder(d, l):
-        """ Returns cylinder's area. Inputs are d = diameter and l = length, both in [m]. Output is in [m^2] """
+        """Returns cylinder's area. Inputs are d = diameter and l = length, both in [m]. Output is in [m^2]"""
         r = d / 2
         area = 2 * np.pi * r * (r + l)
         return area
 
     @staticmethod
     def area_cabin(d, l):
-        """ Returns cabin's area. Inputs are d = diameter and l = length, both in [m]. Output is in [m^2] """
+        """Returns cabin's area. Inputs are d = diameter and l = length, both in [m]. Output is in [m^2]"""
         r = d / 2
         area = 2 * np.pi * r * l
         return area
 
-
     @staticmethod
     def volume_cylinder(d, l):
-        """ Returns cylinder's volume. Inputs are d = diameter and l = length, both in [m]. Output is in [m^3] """
+        """Returns cylinder's volume. Inputs are d = diameter and l = length, both in [m]. Output is in [m^3]"""
         r = d / 2
-        volume = l * np.pi * r ** 2
+        volume = l * np.pi * r**2
         return volume
 
     @staticmethod
     def area_sphere(d):
-        """ Returns sphere's area. Input is d = diameter in [m]. Output is in [m^2]"""
+        """Returns sphere's area. Input is d = diameter in [m]. Output is in [m^2]"""
         r = d / 2
-        area = 4 * np.pi * r ** 2
+        area = 4 * np.pi * r**2
         return area
 
     @staticmethod
     def volume_sphere(d):
-        """ Returns sphere's volume. Input is d = diameter in [m]. Output is in [m^3]"""
+        """Returns sphere's volume. Input is d = diameter in [m]. Output is in [m^3]"""
         r = d / 2
-        area = (4 / 3) * np.pi * r ** 3
+        area = (4 / 3) * np.pi * r**3
         return area
 
 
@@ -111,13 +109,12 @@ fuselage_area = 30.81
 
 fuselage = Fuselage(cabin_width, cabin_height, fuselage_length, fuselage_area)
 
-print(f' Width:{fuselage.width}        Height:{fuselage.height}       Length:{round(fuselage.length, 3)}    Area:{round(fuselage.area, 5)}     cd:{round(fuselage.cd, 5)}')
+print(
+    f" Width:{fuselage.width}        Height:{fuselage.height}       Length:{round(fuselage.length, 3)}    Area:{round(fuselage.area, 5)}     cd:{round(fuselage.cd, 5)}"
+)
 
 v = 111
 rho = 0.01
-drag = round(fuselage.drag_simulation(v, rho),2)
+drag = round(fuselage.drag_simulation(v, rho), 2)
 # drag = round(drag, 3)
 print("drag force: ", drag, "[N]")
-
-
-
--- dse/detailed/testing_body_drag_calculations_v3.py
+++ dse/detailed/testing_body_drag_calculations_v3.py
@@ -11,23 +11,19 @@ it might no be too hard to fix, but if a layer 1 fails, something really
 fundamental might be broken.
 """
 
+
 class TestFuselageLogic(unittest.TestCase):
-
     def test_layer1_object_creation(self):
-
         cabin_width = 1.5
         cabin_height = 1.8
         fuselage_length = 5.72
         fuselage_area = 25.88
 
-
         fus = Fuselage(cabin_width, cabin_height, fuselage_length, fuselage_area)
 
         self.assertIsInstance(fus, Fuselage, "It appears the class assignment is not working")
 
-
     def test_layer3_assign_properties(self):
-
         cabin_width = 1.5
         cabin_height = 1.8
         fuselage_length = 5.72
@@ -36,10 +32,13 @@ class TestFuselageLogic(unittest.TestCase):
         fus = Fuselage(cabin_width, cabin_height, fuselage_length, fuselage_area)
 
         assert_equal_message_firsttest_1 = "The initializer values could not be assigned properly"
-        assert_equal_message_firsttest_2 = "The fuselage's total length is not being calculated properly"
-        assert_equal_message_firsttest_3 = "The fuselage's total surface is not being calculated properly"
+        assert_equal_message_firsttest_2 = (
+            "The fuselage's total length is not being calculated properly"
+        )
+        assert_equal_message_firsttest_3 = (
+            "The fuselage's total surface is not being calculated properly"
+        )
         assert_equal_message_firsttest_4 = "The fuselage's Cd is not being calculated properly"
-
 
         self.assertEqual(fus.width, cabin_width, assert_equal_message_firsttest_1)
         self.assertEqual(fus.length, fuselage_length, assert_equal_message_firsttest_1)
@@ -54,11 +53,7 @@ class TestFuselageLogic(unittest.TestCase):
         self.assertEqual(round(fus.area, 2), 25.88, assert_equal_message_firsttest_3)
         self.assertEqual(round(fus.cd, 5), 0.02941, assert_equal_message_firsttest_4)
 
-
-
-
     def test_layer2_check_methods(self):
-
         cabin_width = 1.5
         cabin_height = 1.8
         fuselage_length = 5.72
@@ -66,38 +61,56 @@ class TestFuselageLogic(unittest.TestCase):
 
         fus = Fuselage(cabin_width, cabin_height, fuselage_length, fuselage_area)
 
-        self.assertEqual(round(fus.calculate_cd(), 5), 0.02941, "The method calculate_cd is failing to calculate correctly.")
-
+        self.assertEqual(
+            round(fus.calculate_cd(), 5),
+            0.02941,
+            "The method calculate_cd is failing to calculate correctly.",
+        )
 
     def test_layer4_check_drag_calculation(self):
-
         cabin_width = 1.5
         cabin_height = 1.8
         fuselage_length = 5.72
         fuselage_area = 25.88
 
         fus = Fuselage(cabin_width, cabin_height, fuselage_length, fuselage_area)
-
 
         v = 111
         rho = 0.01
         drag = fus.drag_simulation(v, rho)
 
-        self.assertEqual(round(drag, 2), 46.88, "The final drag force calculation does not return the correct value")
-
+        self.assertEqual(
+            round(drag, 2),
+            46.88,
+            "The final drag force calculation does not return the correct value",
+        )
 
     def test_static_methods(self):
-
         sphere_diameter = 4
         cylinder_diameter = 2
         cylinder_length = 10
 
-        self.assertEqual(Fuselage.area_cylinder(cylinder_diameter, cylinder_length), 22*np.pi, "Test related to static methods failed")
-        self.assertEqual(Fuselage.volume_cylinder(cylinder_diameter, cylinder_length), 10*np.pi, "Test related to static methods failed")
-        self.assertEqual(Fuselage.area_sphere(sphere_diameter), 16*np.pi, "Test related to static methods failed")
-        self.assertEqual(Fuselage.volume_sphere(sphere_diameter), np.pi*32/3, "Test related to static methods failed")
+        self.assertEqual(
+            Fuselage.area_cylinder(cylinder_diameter, cylinder_length),
+            22 * np.pi,
+            "Test related to static methods failed",
+        )
+        self.assertEqual(
+            Fuselage.volume_cylinder(cylinder_diameter, cylinder_length),
+            10 * np.pi,
+            "Test related to static methods failed",
+        )
+        self.assertEqual(
+            Fuselage.area_sphere(sphere_diameter),
+            16 * np.pi,
+            "Test related to static methods failed",
+        )
+        self.assertEqual(
+            Fuselage.volume_sphere(sphere_diameter),
+            np.pi * 32 / 3,
+            "Test related to static methods failed",
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
